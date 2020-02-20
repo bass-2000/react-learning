9ca024a1b2ac22107fc6c5c246e77b8cc24b4192
alexanderboyko <bass-2000@yandex.ru>
2020-02-21 00:20:22 +0300

    fix of warning with collections without unique key adding css

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,16 +1,22 @@
-import React from "react";
-import TodoListItem from "./todo-list-item";
+import React from 'react';
+
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
 const TodoList = ({todos}) => {
+
     const elements = todos.map((item) => {
-        return (<li>
-            <TodoListItem
-                label={item.label}
-                important={item.important}/>
-        </li>)
+        const {id, ...itemProps} = item;
+
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps} />
+            </li>
+        );
     });
+
     return (
-        <ul>
+        <ul className="list-group todo-list">
             {elements}
         </ul>
     );
--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import ReactDOM from 'react-dom';
 import TodoList from "./components/todo-list";
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
+import ItemStatusFilter from "./components/item-status-filter";
 
-const todoData = [
-    {label: 'Drink coffee', important: false},
-    {label: 'Learn React', important: true},
-    {label: 'Build Awesome App', important: false}
-]
+import './index.css';
 
 const App = () => {
 
-    return (<div>
+    const todoData = [
+        {label: 'Drink coffee', important: false, id: 1},
+        {label: 'Learn React', important: true, id: 2},
+        {label: 'Build Awesome App', important: false, id: 3}
+    ];
 
-        <AppHeader/>
-        <SearchPanel/>
-        <TodoList todos={todoData}/>
-    </div>);
+    return (
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel/>
+                <ItemStatusFilter/>
+            </div>
+
+            <TodoList todos={todoData}/>
+        </div>
+    );
 };
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App/>,
+    document.getElementById('root'));
