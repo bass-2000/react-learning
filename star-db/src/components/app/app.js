f7e9c200fdf7c3a8e0865140e0b4844544d018c2
alexanderboyko <bass-2000@yandex.ru>
2020-02-26 19:43:36 +0300

    Higher order components + refactoring

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -2,22 +2,23 @@ import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ErrorBoundry from "../error-boundry";
+import ErrorBoundry from '../error-boundry';
+import ItemDetails, {Record} from "../item-details/item-details";
+import SwapiService from "../../services/swapi-service";
+
+import ItemList from '../item-list';
 
 import './app.css';
-import ItemDetails, {Record} from "../item-details/item-details";
-import Row from "../row";
-import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component {
 
-  swapiService = new SwapiService();
+    swapiService = new SwapiService();
 
-  state = {
-    showRandomPlanet: true
-  };
+    state = {
+        showRandomPlanet: true
+    };
 
-  toggleRandomPlanet = () => {
+    toggleRandomPlanet = () => {
     this.setState((state) => {
       return {
         showRandomPlanet: !state.showRandomPlanet
@@ -27,59 +28,65 @@ export default class App extends Component {
 
   render() {
 
-    const planet = this.state.showRandomPlanet ?
-        <RandomPlanet/> :
-        null;
+      const planet = this.state.showRandomPlanet ?
+          <RandomPlanet/> :
+          null;
 
-    const {
-      getPerson,
-      getStarship,
-      getPersonImage,
-      getStarshipImage
-    } = this.swapiService;
+      const {
+          getPerson,
+          getStarship,
+          getPersonImage,
+          getStarshipImage,
+          getAllPeople,
+          getAllPlanets
+      } = this.swapiService;
 
-    const personDetails = (
-        <ItemDetails
-            itemId={1}
-            getData={getPerson}
-            getImageUrl={getPersonImage}>
+      const personDetails = (
+          <ItemDetails
+              itemId={11}
+              getData={getPerson}
+              getImageUrl={getPersonImage}>
 
-          <Record field="gender" label="Gender"/>
-          <Record field="eyeColor" label="Eye Color"/>
+              <Record field="gender" label="Gender"/>
+              <Record field="eyeColor" label="Eye Color"/>
 
-        </ItemDetails>
-    );
+          </ItemDetails>
+      );
 
     const starshipDetails = (
         <ItemDetails
-            itemId={11}
+            itemId={5}
             getData={getStarship}
             getImageUrl={getStarshipImage}>
 
-          <Record field="model" label="Model"/>
-          <Record field="length" label="Length"/>
-          <Record field="costInCredits" label="Cost"/>
+            <Record field="model" label="Model"/>
+            <Record field="length" label="Length"/>
+            <Record field="costInCredits" label="Cost"/>
         </ItemDetails>
     );
 
     return (
         <ErrorBoundry>
-          <div className="stardb-app">
-            <Header/>
-            {planet}
+            <div className="stardb-app">
+                <Header/>
 
-            {/*<div className="row mb2 button-row">*/}
-            {/*  <button*/}
-            {/*    className="toggle-planet btn btn-warning btn-lg"*/}
-            {/*    onClick={this.toggleRandomPlanet}>*/}
-            {/*    Toggle Random Planet*/}
-            {/*  </button>*/}
-            {/*  <ErrorButton />*/}
-            {/*</div>*/}
+                <ItemList
+                    getData={getAllPeople}
+                    onItemSelected={() => {
+                    }}>
 
-            {/*<PeoplePage />*/}
-            <Row left={personDetails} right={starshipDetails}/>
-          </div>
+                    {({name}) => <span>{name}</span>}
+                </ItemList>
+
+                <ItemList
+                    getData={getAllPlanets}
+                    onItemSelected={() => {
+                    }}>
+
+                    {({name}) => <span>{name}</span>}
+                </ItemList>
+
+            </div>
         </ErrorBoundry>
     );
   }
--- star-db/src/components/item-details/item-details.js
+++ star-db/src/components/item-details/item-details.js
@@ -6,12 +6,12 @@ import SwapiService from '../../services/swapi-service';
 import './item-details.css';
 
 const Record = ({item, field, label}) => {
-  return (
-      <li className="list-group-item">
-        <span className="term">{label}</span>
-        <span>{item[field]}</span>
-      </li>
-  );
+    return (
+        <li className="list-group-item">
+            <span className="term">{label}</span>
+            <span>{item[field]}</span>
+        </li>
+    );
 };
 
 export {
@@ -32,58 +32,57 @@ export default class ItemDetails extends Component {
     this.updateItem();
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.itemId !== prevProps.itemId) {
-      this.updateItem();
+    componentDidUpdate(prevProps) {
+        if (this.props.itemId !== prevProps.itemId) {
+            this.updateItem();
+        }
     }
-  }
-
 
   updateItem() {
-    const {itemId, getData, getImageUrl} = this.props;
+      const {itemId, getData, getImageUrl} = this.props;
     if (!itemId) {
       return;
     }
 
-    getData(itemId)
-        .then((item) => {
-          this.setState({
-            item,
-            image: getImageUrl(item)
+      getData(itemId)
+          .then((item) => {
+              this.setState({
+                  item,
+                  image: getImageUrl(item)
+              });
           });
-        });
   }
 
   render() {
 
-    const {item, image} = this.state;
-    if (!item) {
-      return <span>Select a item from a list</span>;
-    }
+      const {item, image} = this.state;
+      if (!item) {
+          return <span>Select a item from a list</span>;
+      }
 
-    const {
-      id, name, gender,
-      birthYear, eyeColor
-    } = item;
+      const {
+          id, name, gender,
+          birthYear, eyeColor
+      } = item;
 
-    return (
-        <div className="item-details card">
-          <img className="item-image"
-               src={image}
-               alt="item"/>
+      return (
+          <div className="item-details card">
+              <img className="item-image"
+                   src={image}
+                   alt="item"/>
 
-          <div className="card-body">
-            <h4>{name}</h4>
-            <ul className="list-group list-group-flush">
-              {
-                React.Children.map(this.props.children, (child) => {
-                  return React.cloneElement(child, {item});
-                })
-              }
-            </ul>
-            <ErrorButton/>
+              <div className="card-body">
+                  <h4>{name}</h4>
+                  <ul className="list-group list-group-flush">
+                      {
+                          React.Children.map(this.props.children, (child) => {
+                              return React.cloneElement(child, {item});
+                          })
+                      }
+                  </ul>
+                  <ErrorButton/>
+              </div>
           </div>
-        </div>
     );
   }
 }
