a33cb2ce0d30f7ab528d255bdbb2ae6909df62ed
alexanderboyko <bass-2000@yandex.ru>
2020-03-05 19:11:32 +0300

    use-effect for downloading data without problem

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,17 +1,15 @@
 import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 
-
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
     if (visible) {
         return (
             <div>
                 <button onClick={() => setValue((v) => v + 1)}>+</button>
                 <button onClick={() => setVisible(false)}>hide</button>
-                <HookCounter value={value}/>
-                <Notification/>
+                <PlanetInfo id={value}/>
             </div>
         );
     } else {
@@ -19,23 +17,23 @@ const App = () => {
     }
 };
 
-const HookCounter = ({value}) => {
-    useEffect(() => console.log('mount'), []);
-    useEffect(() => console.log('update'));
-    useEffect(() => () => console.log('unmount'), []);
+const PlanetInfo = ({id}) => {
 
-    return <p> {value} </p>;
-};
-
-const Notification = () => {
-    const [visible, setVisible] = useState(true);
+    const [name, setName] = useState(null);
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            setVisible(false);
-        }, 1500);
-        return () => clearTimeout(timeout);
-    }, []);
-    return <div>{visible && <p>Hello</p>}</div>;
+        let cancelled = false;
+        fetch(`https://swapi.co/api/planets/${id}`)
+            .then(res => res.json())
+            .then(data => !cancelled && setName(data.name));
+        return () => {
+            cancelled = true
+        };
+    }, [id]);
+
+
+    return (
+        <div>{id} - {name}</div>
+    );
 };
 
 ReactDOM.render(<App/>, document.getElementById('root'));
