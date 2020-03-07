7a0780b97be0fecd291789d08c132a6eb7cf93b0
alexanderboyko <bass-2000@yandex.ru>
2020-03-07 10:22:50 +0300

    useCallback & useMemo

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
@@ -18,25 +18,57 @@ const App = () => {
 };
 
 const usePlanetInfo = (id) => {
-    const [name, setName] = useState(null);
+    const request = useCallback(
+        () => getPlanet(id), [id]);
+    return useRequest(request);
+};
+
+const getPlanet = (id) => {
+    return fetch(`https://swapi.co/api/planets/${id}`)
+        .then(res => res.json())
+        .then(data => data);
+};
+
+const useRequest = (request) => {
+    const initialState = useMemo(() => ({
+        data: null,
+        loading: true,
+        error: null
+    }), [])
+    const [dataState, setDataState] = useState(initialState);
     useEffect(() => {
+        setDataState(initialState);
         let cancelled = false;
-        fetch(`https://swapi.co/api/planets/${id}`)
-            .then(res => res.json())
-            .then(data => !cancelled && setName(data.name));
+        request()
+            .then(data => !cancelled && setDataState({
+                data,
+                loading: false,
+                error: null
+            })).catch(error => !cancelled && setDataState({
+            data: null,
+            loading: false,
+            error
+        }));
         return () => {
             cancelled = true
         };
-    }, [id]);
-    return name;
+    }, [request, initialState]);
+    return dataState;
 };
 
 const PlanetInfo = ({id}) => {
 
-    const name = usePlanetInfo(id);
+    const {data, loading, error} = usePlanetInfo(id);
 
+    if (error) {
+        return <div>Something is wrong</div>
+    }
+
+    if (loading) {
+        return <div>loading ...</div>
+    }
     return (
-        <div>{id} - {name}</div>
+        <div>{id} - {data.name}</div>
     );
 };
 
