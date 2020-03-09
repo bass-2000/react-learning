764b6638e44a37bbc7f5b0d7685c855df4b45994
alexanderboyko <bass-2000@yandex.ru>
2020-03-09 15:33:43 +0300

    redux sandbox action creators. bindActionCreators

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,1 +1,33 @@
-console.log('Hello world');
+import {bindActionCreators, createStore} from 'redux';
+
+import reducer from './reducer.js';
+import * as actions from './actions.js';
+
+const store = createStore(reducer);
+const {dispatch} = store;
+
+const {inc, dec, rnd} =
+    bindActionCreators(actions, dispatch);
+
+document
+    .getElementById('inc')
+    .addEventListener('click', inc);
+
+document
+    .getElementById('dec')
+    .addEventListener('click', dec);
+
+document
+    .getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random() * 10);
+        rnd(payload);
+    });
+
+const update = () => {
+    document
+        .getElementById('counter')
+        .innerHTML = store.getState();
+};
+
+store.subscribe(update);
