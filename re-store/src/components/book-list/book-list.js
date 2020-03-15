c2bfa6441e3b5eeee77688522ce941db80548df5
alexanderboyko <bass-2000@yandex.ru>
2020-03-15 13:45:53 +0300

    re-store app thunk middleware

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import BookListItem from '../book-list-item';
 
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import {withBookstoreService} from '../hoc';
@@ -13,21 +14,21 @@ import ErrorIndicator from '../error-indicator';
 import './book-list.css';
 
 const BookList = ({books, onAddedToCart}) => {
-  return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-                <li key={book.id}>
-                  <BookListItem
-                      book={book}
-                      onAddedToCart={() => onAddedToCart(book.id)}/>
-                </li>
-            );
-          })
-        }
-      </ul>
-  );
+    return (
+        <ul className="book-list">
+            {
+                books.map((book) => {
+                    return (
+                        <li key={book.id}>
+                            <BookListItem
+                                book={book}
+                                onAddedToCart={() => onAddedToCart(book.id)}/>
+                        </li>
+                    );
+                })
+            }
+        </ul>
+    );
 };
 
 class BookListContainer extends Component {
@@ -37,14 +38,14 @@ class BookListContainer extends Component {
   }
 
   render() {
-    const {books, loading, error, onAddedToCart} = this.props;
+      const {books, loading, error, onAddedToCart} = this.props;
 
     if (loading) {
-      return <Spinner/>;
+        return <Spinner/>;
     }
 
     if (error) {
-      return <ErrorIndicator/>;
+        return <ErrorIndicator/>;
     }
 
     return <BookList books={books} onAddedToCart={onAddedToCart}/>;
@@ -52,15 +53,15 @@ class BookListContainer extends Component {
 }
 
 const mapStateToProps = ({bookList: {books, loading, error}}) => {
-  return {books, loading, error};
+    return {books, loading, error};
 };
 
 const mapDispatchToProps = (dispatch, {bookstoreService}) => {
 
-  return {
-    fetchBooks: fetchBooks(bookstoreService, dispatch),
-    onAddedToCart: (id) => dispatch(bookAddedToCart(id))
-  };
+    return bindActionCreators({
+        fetchBooks: fetchBooks(bookstoreService),
+        onAddedToCart: bookAddedToCart
+    }, dispatch);
 };
 
 export default compose(
