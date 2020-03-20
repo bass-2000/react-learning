e2578ab06cc1a746f982324e736a0981d64d2d05
alexanderboyko <bass-2000@yandex.ru>
2020-03-20 18:41:59 +0300

    configuration for production and development modes

--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -1,47 +1,66 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-module.exports = {
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.png$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name]-[sha1:hash:7].[ext]'
+module.exports = (env = {}) => {
+    const {mode = 'development'} = env;
+    const isProd = mode === 'production';
+    const isDev = mode === 'development';
+    const getStyleLoaders = () => {
+        return [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+        ];
+    };
+
+    const getPlugins = () => {
+        const plugins = [
+            new HtmlWebpackPlugin({
+                title: 'WebPack plugin builded',
+                buildTime: new Date().toISOString(),
+                template: 'public/index.html'
+            })];
+
+        if (isProd) {
+            plugins.push(new MiniCssExtractPlugin({
+                filename: 'main-[hash:8].css'
+            }));
+        }
+        return plugins;
+    };
+    return {
+        mode: isProd ? 'production' : isDev && 'development',
+        output: isProd ? 'main-[hash:8].js' : undefined,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.png$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.(css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(s[ca]ss)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'WebPack plugin builded',
-            buildTime: new Date().toISOString(),
-            template: 'public/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'main-[hash:8].css'
-        })
-    ],
-    devServer: {
-        open: true
+                    ]
+                },
+                {
+                    test: /\.(css)$/,
+                    use: getStyleLoaders()
+                },
+                {
+                    test: /\.(s[ca]ss)$/,
+                    use: [...getStyleLoaders(), 'sass-loader']
+                }
+            ]
+        },
+        plugins: getPlugins(),
+        devServer: {
+            open: true
+        }
     }
 };
